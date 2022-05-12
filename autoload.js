7a35d836600e936d6074d58d395de8c4ead7ce91
Kylo <eargon.skyle@gmail.com>
2022-05-12 19:41:03 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://github.com/Siriusq/live2d-widget/tree/master/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/Siriusq/live2d-widget@2.0/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
